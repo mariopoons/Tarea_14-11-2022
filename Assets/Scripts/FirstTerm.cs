29b5804dd14698c4c16985b5fcc26482bae42db8
mariopoons <mariopons02@gmail.com>
2022-11-11 18:39:21 +0100

    Inicio Tarea
    esta todo mal pero empezado

---
+++ Assets/Scripts/FirstTerm.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTerm : MonoBehaviour
+{
+    public float 4f;
+    public float 2f;
+    public float 5f;
+    public float 11f;
+    public string "mario";
+    public string "palma";
+    public string "personName";
+    public int 20;
+
+    private void Start()
+    {
+        Debug.Log($"Hello {name}, Welcome to {city}");
+        Debug.Log($"Hello {name}, You are 2 ")
+    }
+
+
+    private void Update()
+    {
+
+    }
+}
